a0492515971cc308914903e33ea42a0dd84f6dfb
Luke-123 <80544939+Luke-123@users.noreply.github.com>
2021-09-24 16:42:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# coding:utf-8
+import cv2
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
-from kivy.graphics import *
-import cv2
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
@@ -23,25 +21,29 @@ class CamApp(App):
 
     def update(self, dt):
         # display image from cam in opencv window
-        out = self.CV()
-        buf1 = cv2.flip(out, 0)
+        ret, frame = self.capture.read()
+        # convert to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+        
+        # draw box around ell the facecv2.VideoCapture
+        for (x, y, w, h) in faces:
+            # draw rectangle
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 4)
+            # get the pixels from gray image (the face image)
+            roi_gray = gray[y:y + w, x:x + w]
+            # get the pixels from the colour image
+            roi_color = frame[y:y + w, x:x + w]
+
+        #convert to texture
+        buf1 = cv2.flip(frame, 0)
         buf = buf1.tostring()
-        texture1 = Texture.create(size=(out.shape[1], out.shape[0]), colorfmt='bgr')
+        texture1 = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
 
         texture1.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
 
         self.img1.texture = texture1
-
-    def CV(self):
-        ret, frame = self.capture.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # detect faces
-        faces = face_cascade.detectMultiScale(gray, 1.2, 5)
-
-
-
-
-
 
 if __name__ == '__main__':
     CamApp().run()
